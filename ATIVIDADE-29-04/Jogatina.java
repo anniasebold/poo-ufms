69cb2d4ed0f04b3643f9d7811f372b81ca4d52e8
anniasebold <anniasebold3.0@gmail.com>
2022-05-05 23:39:58 -0300

    CONTINUACAO ATIVIDADE 29-04

--- ATIVIDADE-29-04/Jogatina.java
+++ ATIVIDADE-29-04/Jogatina.java
@@ -3,27 +3,22 @@ import java.util.Scanner;
 public class Jogatina {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    
-    // Versao 1.0
-    // Cadastro de apostas
-    // (1) Cadastrar aposta de cavalo (vetor de apostas de cavalo)
-    // (2) Cadastrar aposta do bicho (vetor de apostas do bicho)
-    // (3) Listar apostas
-    // (4) Sair
 
     ApostaDoBicho apostasB[] = new ApostaDoBicho[2];
     ApostaDeCavalo apostasC[] = new ApostaDeCavalo[2];
     int opcaoVersao;
     int opcao;
+    
     do {
-      int contB = 0;
-      int contC = 0;
       System.out.printf("Digite a versão (1) , (2) ou (3) p/sair: ");
       opcaoVersao = sc.nextInt();
       switch (opcaoVersao) {
+        // VERSÃO 1.0
         case 1:
+          int contB = 0;
+          int contC = 0;
           do {
-            System.out.println("\nMENU ");
+            System.out.println("\nMENU VERSÃO 1");
             System.out.println("(1)  Cadastrar aposta de cavalo");
             System.out.println("(2) Cadastrar aposta do bicho");
             System.out.println("(3) Listar apostas");
@@ -85,8 +80,69 @@ public class Jogatina {
             }
           } while(opcao != 4);
           break;
+          // VERSÃO 2.0
           case 2:
-            System.out.println("-----------------------------------");
+            Aposta apostas[] = new Aposta[4];
+            int cont = 0;
+            do {
+              System.out.println("\nMENU VERSÃO 2");
+              System.out.println("(1)  Cadastrar aposta de cavalo");
+              System.out.println("(2) Cadastrar aposta do bicho");
+              System.out.println("(3) Listar apostas");
+              System.out.println("(4) Sair");
+              System.out.printf("Digite uma opção: ");
+              opcao = sc.nextInt();
+              switch(opcao) {
+                case 1:
+                  if(cont < apostas.length) {
+                    System.out.println("\nCadastro de Aposta de CAVALO");
+                    System.out.println("Digite o Número do Cavalo e Valor");
+                    apostas[cont] = new ApostaDeCavalo(sc.nextInt(), sc.nextDouble());
+                    cont ++;
+                  } else {
+                    System.out.println("\nVocê atingiu o valor máximo de apostas de cavalo!\n");
+                  }
+                  break;
+                case 2:
+                  if(cont < apostas.length) {
+                    System.out.println("\nCadastro de Aposta de BICHO");
+                    System.out.println("Digite a Dezena 1, Dezena 2 e Valor");
+                    apostas[cont] = new ApostaDoBicho(sc.nextInt(), sc.nextInt(), sc.nextDouble());
+                    cont ++;
+                  } else {
+                    System.out.println("\nVocê atingiu o valor máximo de apostas de bicho!\n");
+                  }
+                  break;
+                case 3:
+                  // Listando Apostas
+                  System.out.println("\nLISTANDO APOSTAS: ");
+                  for(int i = 0; i < apostas.length; i++) {
+                    if(apostas[i] != null) {
+                      if(apostas[i] instanceof ApostaDoBicho) {
+                        ApostaDoBicho temp = (ApostaDoBicho)apostas[i];
+                        System.out.println("\n Apostas de Bicho: ");
+                        System.out.println("Dezena 1: " + temp.getDezena1());
+                        System.out.println("Dezena 2: " + temp.getDezena2());
+                        System.out.println("Valor: " + temp.getValor());
+                        System.out.println("-----------------------------------");
+                      } else if(apostas[i] instanceof ApostaDeCavalo) {
+                        ApostaDeCavalo temp = (ApostaDeCavalo)apostas[i];
+                        System.out.println("\nApostas de Cavalo: ");
+                        System.out.println("Dezena 1: " + temp.getNumeroDoCavalo());
+                        System.out.println("Valor: " + temp.getValor());
+                        System.out.println("-----------------------------------");
+                      }
+                    }
+                  }
+                  break;
+                case 4: 
+                  System.out.println("Até a próxima!");
+                  break;
+                default:
+                  System.out.println("Opção inválida.");
+                  break;
+              }
+            } while(opcao != 4);
             break;
           case 3: 
             System.out.println("Até a próxima!");
@@ -96,12 +152,5 @@ public class Jogatina {
             break;
       }
     } while(opcaoVersao != 3);
-    // Versão 2.0
-    // Aposta aposta[] = new Aposta[10];
-    // Cadastro de apostas
-    // (1) Cadastrar aposta de cavalo (vetor de apostas)
-    // (2) Cadastrar aposta do bicho (vetor de apostas)
-    // (3) Listar apostas
-    // (4) Sair
   }
 }
