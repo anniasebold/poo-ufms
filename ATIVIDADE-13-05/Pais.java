30ea30193e1cb8fbb5987b5bf0b033f6abcc34e2
anniasebold <anniasebold3.0@gmail.com>
2022-05-22 21:15:53 -0300

    Continuacao ATIVIDADE-13-05

--- ATIVIDADE-13-05/Pais.java
+++ ATIVIDADE-13-05/Pais.java
@@ -1,3 +1,115 @@
 class Pais {
+  private String codIso;
+  private String nome;
+  private int populacao;
+  private double dimensao;
+  Pais paisesFronteiras[] = new Pais[20];
+  int cont = 0;
+  private static int contador;
 
+  public Pais(String codISO, String nome, double dimensao) {
+    setCodIso(codISO);
+    setNome(nome);
+    setDimensao(dimensao);
+    setContador();
+  }
+
+  public static int getContador() {
+    return contador;
+  }
+
+  public static void setContador() {
+    contador = getContador() + 1;
+  }
+
+
+  public void setCodIso(String codIso) {
+    this.codIso = codIso;
+  }
+
+  public String getCodIso() {
+    return this.codIso;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setPopulacao(int populacao) {
+    this.populacao = populacao;
+  }
+
+  public int getPopulacao() {
+    return this.populacao;
+  }
+
+  public void setDimensao(double dimensao) {
+    this.dimensao = dimensao;
+  }
+
+  public double getDimensao() {
+    return this.dimensao;
+  }
+
+  public void setPaisFronteira(Pais pais) {
+    if(paisesFronteiras[cont] == null) {
+      paisesFronteiras[cont] = pais;
+    }
+    cont++;
+  }
+
+  public void getPaisesFronteira() {
+    for(int i = 0; i < paisesFronteiras.length; i++) {
+      if(paisesFronteiras[i] != null) {
+        System.out.println("País " + i);
+        System.out.println(paisesFronteiras[i].getCodIso());
+        System.out.println(paisesFronteiras[i].getNome());
+      }
+    }
+  }
+
+  public boolean fazFronteira(Pais pais) {
+    for(int i = 0; i < paisesFronteiras.length; i++) {
+      if(paisesFronteiras[i] != null) {
+        if(paisesFronteiras[i].equals(pais)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  public double getDensidadePopulacional() {
+    return getPopulacao() / getDimensao();
+  }
+
+  public Pais[] listPaisesVizinhos(Pais pais) {
+    int cont = 0;
+    Pais paisesVizinhos[] = new Pais[20];
+    for(int i = 0; i < paisesFronteiras.length; i++) {
+      for(int y = 0; y < pais.paisesFronteiras.length; i++) {
+        if(paisesFronteiras[i] != null && pais.paisesFronteiras[y] != null) {
+          System.out.println(paisesFronteiras[i].getCodIso() + " " + pais.paisesFronteiras[y].getCodIso());
+          System.out.println("entrou 1");
+          if(paisesFronteiras[i].equals(pais.paisesFronteiras[y])) {
+            System.out.println("entrou");
+            System.out.println(paisesFronteiras[i].getCodIso() + " " + pais.paisesFronteiras[y].getCodIso());
+            paisesVizinhos[cont] = paisesFronteiras[i];
+            cont++;
+          }
+        }
+      }
+    }
+
+    return paisesVizinhos;
+  }
+
+  public boolean equals(Pais p) {
+    return getCodIso().equals(p.getCodIso());
+  }
 }
---
+++ ATIVIDADE-13-05/TestePais.java
@@ -0,0 +1,26 @@
+public class TestePais {
+    public static void main(String[] args) {
+      Pais pais1 = new Pais("BRA", "Brasil", 10000);
+      pais1.setPopulacao(15000);
+
+
+      Pais pais2 = new Pais("ARG", "Argentina", 1000);
+      pais2.setPopulacao(1500);
+
+      Pais pais3 = new Pais("CHI", "Chile", 100);
+      pais3.setPopulacao(150);
+
+
+      pais1.setPaisFronteira(pais2);
+      pais1.setPaisFronteira(pais3);
+      pais1.getPaisesFronteira();
+
+      pais2.setPaisFronteira(pais3);
+      pais2.getPaisesFronteira();
+
+      pais1.listPaisesVizinhos(pais2);
+
+      // System.out.println(pais1.fazFronteira(pais2));
+      // System.out.println(Pais.getContador());
+    } 
+}
